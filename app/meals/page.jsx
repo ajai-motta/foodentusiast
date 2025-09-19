b799ff006ca21f69f56e9936299fe9448ad5a84b
Ajai Joseph <ajayjoseo8@gmail.com>
2025-09-19 11:52:37 +0530

    database

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import classes from './page.module.css'
+import Link from 'next/link'
+import MealGrid from '@/components/meals/meal-grid'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <header className={classes.header}>
+        <h1>Delicus meals created {' '} <span>by you</span></h1>
+        <p>Choose you favoriate recipe and cook it your self.</p>
+        <p className={classes.cta}><Link href="/meals/share">Share your Most beloved recipe</Link></p>
+      </header>
+      <main className={classes.main}>
+        <MealGrid meals={[]}/>
+
+      </main>
+    </div>
   )
 }
 
